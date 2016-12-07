d4d33d88dc1872b6db993e7646404c2e5f56027f
Bojan Lazic <lazicbojanns@gmail.com>
2016-12-07 22:53:18 +0100

    Make inject task

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,26 @@ htmlmin        = require('gulp-htmlmin');
 
 require('gulp-task-loader-recursive')(gulp);
 
+// Inject Bower
+gulp.task('bower', ['build:css'], injectBower);
 
-gulp.task('default', ['remove', 'build:images','build:fonts', 'build:sprite', 'build:javaScript', 'build:css', 'build:jade', 'build:twig']);
+function injectBower() {
+  var target = gulp.src('./src/index.html');
+  var js     = gulp.src(wiredep().js);
+  var css    = gulp.src(wiredep().css);
+
+  return target
+  .pipe(inject(js.pipe(concat('bower.js'))
+  .pipe(uglify())
+  .pipe(rev())
+  .pipe(gulp.dest('./dest/js'))))
+
+  .pipe(inject(css.pipe(concat('bower.css'))
+  .pipe(csso())
+  .pipe(rev())
+  .pipe(gulp.dest('./dest/css'))))
+
+  .pipe(gulp.dest('./dest'));
+};
+
+gulp.task('default', ['remove', 'build:images','build:fonts', 'build:sprite', 'build:javaScript', 'build:css', 'build:jade', 'build:twig', 'build:inject']);
---
+++ tasks/build/inject.js
@@ -0,0 +1,16 @@
+var gulp                    = require('gulp'),
+inject                      = require('gulp-inject');
+
+
+module.exports              = function() {
+
+    var target              = gulp.src('./dest/index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources             = gulp.src(['./dest/js/**/*.js', './dest/css/**/*.css'], {read: false});
+
+    return target.pipe(inject(sources))
+      .pipe(gulp.dest('./dest'));
+
+};
+
+module.exports.dependencies = [ 'remove', 'build:twig', 'build:css', 'build:javaScript' ];
