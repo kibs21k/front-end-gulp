b491c57bd69fabdb2dc9f67dbafef499e04f922a
Bojan Lazic <lazicbojanns@gmail.com>
2016-12-07 01:20:53 +0100

    Make js minify && coffeescript

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,4 @@ htmlmin        = require('gulp-htmlmin');
 require('gulp-task-loader-recursive')(gulp);
 
 
-gulp.task('default', ['remove', 'build:images','build:fonts', 'build:sprite']);
+gulp.task('default', ['remove', 'build:images','build:fonts', 'build:sprite', 'build:javaScript']);
---
+++ tasks/build/javaScript.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+coffee = require('gulp-coffee'),
+gutil = require('gulp-util'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+sourcemaps = require('gulp-sourcemaps');
+
+module.exports            = function() {
+
+  gulp.src('src/js/**/*.{js,coffee}')
+    .pipe(sourcemaps.init())
+    .pipe(coffee({bare: true})
+    .on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dest/js'));
+
+};
+
+module.exports.dependencies = [ 'remove' ];
